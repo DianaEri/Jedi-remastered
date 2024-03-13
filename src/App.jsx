cbdb286a93f7c9203fd6ec85eb5b2a54188d246b
DianaEri <153087653+DianaEri@users.noreply.github.com>
2024-03-13 17:18:08 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useState } from 'react';
+import jediLogo from './assets/jediLogo.png';
+import './App.css';
+import SearchBar from './SearchBar';
+
+const FilmList = ({ list }) => {
+  return (
+    <ul>
+      {list.map((film) => (
+        <ul>
+          <li>
+            <span>Title:</span> {film.title}
+          </li>
+          <li>
+            <span>Director:</span> {film.director}
+          </li>
+          <li>
+            <span>Producer:</span> {film.producer}
+          </li>
+          <li>
+            <span>Opening Crawl:</span> {film.opening_crawl}
+          </li>
+          <li>
+            <span>Release Date:</span> {film.release_date}
+          </li>
+        </ul>
+      ))}
+    </ul>
+  );
+};
+
+// React funktion komponenten Filmlist loopar igenom en array lista
+// för att hämta specifika nycklar och lägga in de i en onumrerad lista.
+// Sökresultatet presenteras i text direkt under sökrutan.
+
+function App() {
+  const [filmResult, setFilmResult] = useState([]);
+
+  return (
+    <div>
+      <img src={jediLogo} className="logo react" alt="Jedi logo" />
+      <h1>The Jedi Archive</h1>
+      <p className="read-the-docs">
+        Use the search bar to access Star Wars data.
+      </p>
+      <SearchBar callback={setFilmResult} />
+      <FilmList list={filmResult} />
+    </div>
+  );
+}
+
+// React funkion komponent App returnerar en div som innehåller
+// html-komponenter som img, h1, p och två HTML-block som innehåller
+// React funktion komponent “Searchbar” och React funktion komponent
+// “Filmlist”.
+
+export default App;
---
+++ src/SearchBar.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+
+let SWAPI_URI = 'https://swapi.info/api/films/';
+
+const SearchBar = (args) => {
+  const [value, setValue] = useState(''); // Text bar input
+
+  const logData = (data) => {
+    console.log('Data fetched...');
+    console.log(data);
+  };
+
+  const logValue = (value) => {
+    console.log('The Value is: ' + value);
+  };
+
+  const searchFilm = (value, films) => {
+    const propsToMatch = [
+      'title',
+      'director',
+      'opening_crawl',
+      'producer',
+      'release_date',
+    ];
+
+    let filmResult = [];
+
+    value = value.trim();
+    if (value.length < 1) {
+      films = [];
+    }
+
+    for (const film of films) {
+      for (const prop of propsToMatch) {
+        if (prop in film) {
+          if (film[prop].toLowerCase().indexOf(value.toLowerCase()) != -1) {
+            console.log(film.title);
+            console.log('Matched by: ' + prop);
+            filmResult.push(film);
+            break;
+          }
+        }
+      }
+
+      if (filmResult.length == 1) {
+        break;
+      }
+    }
+
+    args.callback(filmResult);
+  };
+
+  useEffect(() => {
+    fetch(SWAPI_URI)
+      .then((response) => response.json())
+      .then((films) => searchFilm(value, films));
+  }, [value]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search.."
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+// React funktion komponent “Searchbar” har som roll att ta värdet
+// från textfältet och jämföra det med API Films och hitta matchningar.
+// Den är skiftlägesokänslig och söker igenom filmtitlar, producenter,
+// regissörer, utgivningsdatum och filmsammanfattning. React funktion
+// komponent “Searchbar” använder React props för att matcha värdet med
+// API. Så nyckeln från API har gjorts om till en React Props så att
+// funktionen kan läsa den som ett argument. Min React Props heter
+// “prop”, och är en sträng (string). Jag skapade en if-funktion inuti
+// en if-funktion, inuti en for-funktion som i sin tur är inuti en
+// for-funktion.
+
+export default SearchBar;
